43459da51ffb9221be4b65952bc77fbd1277fb2a
sawd1208 <sawdchien@gmail.com>
2023-05-05 01:20:45 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
+// 加入這段 code, 僅在非正式環境時，使用dotenv
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const app = express()
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })  //設定連線到 mongoDB
+
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello world!!!</h1>')
