115cd9569727ee11dcceb7969fa5d76b2f7ab16d
sawd1208 <sawdchien@gmail.com>
2023-05-07 12:02:58 +0800

    feat: motify index.hbs

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
+const Todo = require('./models/todo')
 
 // 加入這段 code, 僅在非正式環境時，使用dotenv
 if (process.env.NODE_ENV !== 'production') {
@@ -25,7 +26,10 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs'}))
 app.set('view engine', 'hbs')
 
 app.get('/', (req, res) => {
-  res.render('index')
+  Todo.find() //取出 Todo model 的所有資料
+    .lean() //把Mongoose 的 model 物件轉換成乾淨的 javascript 資料陣列
+    .then(todos => res.render('index', { todos })) //將資料傳給 index 模板
+    .catch(error => console.error(error)) //處理錯誤
 })
 
 app.listen(3000, () => {
