b82dcc4d093d5bcd5e4404533ab51bf7f2131fb8
sawd1208 <sawdchien@gmail.com>
2023-05-12 12:25:58 +0800

    feat: add sort function

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.get('/', (req, res) => {
   Todo.find() //取出 Todo model 的所有資料
     .lean() //把Mongoose 的 model 物件轉換成乾淨的 javascript 資料陣列
+    .sort({  _id: 'asc' })  // asc升序排列 ，desc降序排列
     .then(todos => res.render('index', { todos })) //將資料傳給 index 模板
     .catch(error => console.error(error)) //處理錯誤
 })
