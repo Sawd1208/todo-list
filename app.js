928ae594c52353feaefbe274b2fd41277804ed6e
sawd1208 <sawdchien@gmail.com>
2023-05-13 00:09:32 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,38 +1,16 @@
 const express = require('express')
-const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
-
-const Todo = require('./models/todo')
 // 引用路由器
 const routes = require('./routes')
+require('./config/mongoose')
 
 const app = express()
 
-// 加入這段 code, 僅在非正式環境時，使用dotenv
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
-
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })  //設定連線到 mongoDB
-
-// 取得資料庫連線狀態
-const db = mongoose.connection
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-// 連線成功
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
-
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs'}))
 app.set('view engine', 'hbs')
-
 app.use(bodyParser.urlencoded({ extended: true }))
-
 app.use(methodOverride('_method'))
 
 app.use(routes) // 將 request 導入路由器
