38f49bfb405e1304592fb52442d7831b96c9fc28
sawd1208 <sawdchien@gmail.com>
2023-05-12 22:55:09 +0800

    refactor: modify express routes

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 
 const Todo = require('./models/todo')
+// 引用路由器
+const routes = require('./routes')
 
 const app = express()
 
@@ -12,7 +14,6 @@ const app = express()
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
-
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })  //設定連線到 mongoDB
 
@@ -34,68 +35,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(methodOverride('_method'))
 
-app.get('/', (req, res) => {
-  Todo.find() //取出 Todo model 的所有資料
-    .lean() //把Mongoose 的 model 物件轉換成乾淨的 javascript 資料陣列
-    .sort({  _id: 'asc' })  // asc升序排列 ，desc降序排列
-    .then(todos => res.render('index', { todos })) //將資料傳給 index 模板
-    .catch(error => console.error(error)) //處理錯誤
-})
-
-app.get('/todos/new', (req, res) => {
-  return res.render('new')
-})
-
-app.get('/todos/:id', (req, res) => {
-  const id = req.params.id
-  return Todo.findById(id)
-    .lean()
-    .then(todo => res.render('detail', { todo }))
-    .catch(error => console.log(error))
-})
-
-app.get('/todos/:id/edit', (req, res) => {
-  const id = req.params.id
-  return Todo.findById(id)
-    .lean()
-    .then(todo => res.render('edit', { todo }))
-    .catch(error => console.log(error))
-})
-
-app.post('/todos', (req, res) => {
-  const name = req.body.name //從req.body 拿出表單裡的name資料
-  // 作法一
-  return Todo.create({ name })  //存入資料庫
-    .then(() => res.redirect('/')) //新增完成後導回首頁
-    .catch(error => console.log(error))
-
-  // // 作法二
-  // const todo = new Todo({ name })  ////從Todo 產生一個實例
-  // return todo.save()  //將該實例存入資料庫
-  //   .then(() => res.redirect('/'))
-  //   .catch(error => console.log(error))  
-})
-
-app.put('/todos/:id', (req, res) => {
-  const id = req.params.id
-  const { name, isDone } = req.body
-  return Todo.findById(id)
-    .then(todo => {
-      todo.name = name
-      todo.isDone = isDone === 'on'
-      return todo.save()
-    })
-    .then(() => res.redirect(`/todos/${id}`))
-    .catch(error => console.log(error))
-})
-
-app.delete('/todos/:id', (req, res) => {
-  const id = req.params.id
-  return Todo.findById(id)
-    .then(todo => todo.remove())
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
-})
+app.use(routes) // 將 request 導入路由器
 
 app.listen(3000, () => {
   console.log('App is running on http://localhost:3000')
