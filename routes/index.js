f35008126ed20eac835f613294551f2a8bfe7a72
sawd1208 <sawdchien@gmail.com>
2023-06-01 14:03:49 +0800

    add user modules

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,12 @@
-// 引用 Express 與 Express 路由器
 const express = require('express')
 const router = express.Router()
 
-// 引入 home 模組程式碼
 const home = require('./modules/home')
-// 引入 todos 模組程式碼
 const todos = require('./modules/todos')
-// 將網址結構符合 / 字串的 request 導向 home 模組
-router.use('/', home)
-// 將網址結構符合 /todos 字串開頭的 request 導向 todos 模組
-router.use('/todos', todos)
+const users = require('./modules/users')
 
-// 準備引入路由模組
-// 會出路由器
+router.use('/', home)
+router.use('/todos', todos)
+router.use('/users', users)
+
 module.exports = router
