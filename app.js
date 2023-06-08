a89bed260c4ab0a210e8e9fe91e304623747d8a2
sawd1208 <sawdchien@gmail.com>
2023-06-08 15:17:28 +0800

    feat: add message partials

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const session = require('express-session')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+const flash = require('connect-flash')
 
 const routes = require('./routes')
 
@@ -25,7 +26,16 @@ app.use(session({
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 // 呼叫 Passport 函數並傳入 app，這條要寫在路由之前
+
 usePassport(app)
+app.use(flash())
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
+  next()
+})
 
 app.use(routes) // 將 request 導入路由器
 
--- models/todo.js
+++ models/todo.js
@@ -2,12 +2,18 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const todoSchema = new Schema({
   name: {
-    type: String, //資料型別是字串
-    require: true, //必填欄位 
+    type: String, 
+    require: true,
   },
   isDone: {
     type: Boolean,
     default: false //預設狀態為 false
+  },
+  userId: {  // 加入關聯欄位
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    index: true,
+    required: true
   }
 })
 module.exports = mongoose.model('Todo', todoSchema)
